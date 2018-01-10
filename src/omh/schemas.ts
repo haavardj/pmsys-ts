c35167cf5ea0ad11c5122d96de49e3aabb1feb05
Håvard Johansen <haavardj@cs.uit.no>
2018-01-10 15:32:41 +0100

    feat(schemas): Add SchemaVerion class

--- src/omh/schemas.ts
+++ src/omh/schemas.ts
@@ -2,11 +2,20 @@
 export interface ISchemaID {
     namespace: string;
     name: string;
-    version: string;
+    version: ISchemaVersion;
+}
+
+export interface ISchemaVersion {
+    major: number;
+    minor: number;
+}
+
+export class SchemaVersion implements ISchemaVersion{
+    constructor(public major: number, public minor:number) {}
 }
 
 export class SchemaID implements ISchemaID {
-    constructor(public namespace: string, public name: string, public version: string){};
+    constructor(public namespace: string, public name: string, public version: ISchemaVersion) {};
 }
 
 
